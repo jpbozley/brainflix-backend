a85ad872dd6eebb66cba3e3ba3b176032f0a8ca7
John Bosley <johnbosley@Johns-MacBook-Air.local>
2022-08-31 11:27:07 -0400

    updated upload data

--- routes/videos.js
+++ routes/videos.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { appendFile } = require("fs");
 
 const router = express.Router();
 
@@ -7,7 +8,7 @@ const videosData = require('../data/videos-data.json')
 const videosSimple = require('../data/videos.json')
 
 
-
+router.use(express.json());
 
 router.get('/', (req, res) => {
     res.send(videosSimple)
@@ -26,28 +27,47 @@ router.get('/:id', (req, res) => {
 router.post('/', (req, res) => {
 
     const newVidSimple = {
-        title: "testing",
+        title: req.body.title,
         channel: "John",
         image: "https://i.imgur.com/5qyCZrD.jpg",
         id: "4"
     }
 
     const newVid = {
-        title: "Test Video",
+        title: req.body.title,
         channel: "John",
         image: "https://i.imgur.com/5qyCZrD.jpg",
         id: "4",
-        description: "This is a test video",
+        description: req.body.description,
         views: `100`,
         likes: `100`,
         duration: "3:00",
         timestamp: "1630656720000",
-        comments: [],
+        comments: [
+            {
+                "name": "John Basedow",
+                "comment": "This video stopped me from working on my biceps, triceps, and even abs.",
+                "likes": 0,
+                "timestamp": 1632227521000
+            },
+            {
+                "name": "Tony Robbins",
+                "comment": "This video is so inspiring.",
+                "likes": 0,
+                "timestamp": 1632227521000
+            },
+            {
+                "name": "Jeff Lebowski",
+                "comment": "This video is a bummer, man.",
+                "likes": 0,
+                "timestamp": 1632227521000
+            }
+        ],
 
     }
     videosSimple.push(newVidSimple);
     videosData.push(newVid);
-    res.json(newVidSimple);
+    console.log(req.body);
 })
 
 
